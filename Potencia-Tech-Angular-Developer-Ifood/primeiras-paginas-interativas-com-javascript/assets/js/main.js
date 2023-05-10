8436be207458f3354efd54365aab0df3f5b735b4
eaeniko <eaenikolai@gmail.com>
2023-05-10 20:07:25 -0300

    dynamic html with the api response

--- Potencia-Tech-Angular-Developer-Ifood/primeiras-paginas-interativas-com-javascript/assets/js/main.js
+++ Potencia-Tech-Angular-Developer-Ifood/primeiras-paginas-interativas-com-javascript/assets/js/main.js
@@ -2,8 +2,33 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+  return `
+	<li class="pokemon">
+	<span class="pokemon-number">#001</span>
+	<span class="pokemon-name">${pokemon.name}</span>
+
+	<div class="pokemon-detail">
+		<ol class="pokemon-types">
+			<li class="pokemon-type">grass</li>
+			<li class="pokemon-type">poison</li>
+		</ol>
+
+		<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+			alt="${pokemon.name}">
+	</div>
+	</li>
+  `
+}
+const pokemonOlList = document.getElementById('pokemons-list');
 fetch(url)
   .then((response) => response.json())
-  .then((jsonBody) => console.log(jsonBody))
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemonList) => {
+	for (let i = 0; i < pokemonList.length; i++) {
+		const pokemon = pokemonList[i];
+		pokemonOlList.innerHTML += convertPokemonToLi(pokemon);
+	}
+  })
   .catch((error) => console.log(error))
   .finally(() => console.log('Concluido'))
